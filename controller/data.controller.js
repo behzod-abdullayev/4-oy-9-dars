b273776b4cee1cee1c6a8e2097acc767137740aa
behzod07-abdullayev <bekzod2366@gmail.com>
2025-12-04 19:28:41 +0500

    4-oy-9-dars

--- controller/data.controller.js
+++ controller/data.controller.js
@@ -114,10 +114,143 @@ const deleteFood = async (req, res) => {
   }
 };
 
+
+
+//                                                  qoshimcha
+
+//                                                         cities
+// get
+
+const getAllcities = async (req, res) => {
+  try {
+    const cities = read_file("largestcities.json");
+    res.status(200).json(cities);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// get one
+
+const getOnecity = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cities = read_file("largestcities.json");
+    const foundcities = cities.find((item) => item.id === id);
+
+    if (!foundcities) {
+      return res.status(404).json({
+        message: "cities not found",
+      });
+    }
+    res.status(200).json(foundcities);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+//post
+
+const addcities = async (req, res) => {
+  try {
+    const { name, area, population, rank} = req.body;
+    const fileData = read_file("largestcities.json");
+    fileData.push({
+      id: v4(),
+      name,
+      area,
+      population,
+      rank
+    });
+
+    write_file("largestcities.json", fileData);
+    res.status(201).json({
+      message: "added new cities",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+//put
+
+const updatecities = async (req, res) => {
+  try {
+    const { name, area, population, rank} = req.body;
+    const { id } = req.params;
+    const cities = read_file("largestcities.json");
+    const foundcities = cities.find((item) => item.id === id);
+    if (!foundcities) {
+      return res.status(404).json({
+        message: "cities not found",
+      });
+    }
+    cities.forEach((item) => {
+      if (item.id === id) {
+        item.name = name ? name : item.name;
+        item.area = area ? area : item.area;
+        item.population = population ? population : item.population;
+        item.rank = rank ? rank : item.rank;
+      }
+    });
+
+    write_file("largestcities.json", cities);
+    res.status(200).json({
+      message: "updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+//delete
+
+const deletecities = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cities = read_file("largestcities.json");
+    const foundcities = cities.find((item) => item.id === id);
+    if (!foundcities) {
+      return res.status(404).json({
+        message: "cities not found",
+      });
+    }
+    cities.forEach((item, idx) => {
+      if (item.id === id) {
+        cities.splice(idx, 1);
+      }
+    });
+
+    write_file("largestcities.json", cities);
+    res.status(200).json({
+      message: "deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
+
 module.exports = {
   getAllFood,
   getOneFood,
   updateFood,
   addFood,
   deleteFood,
+  getAllcities,
+  getOnecity,
+  addcities,
+  updatecities,
+  deletecities
 };
--- router/data.routes.js
+++ router/data.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require("express")
-const { getAllFood, getOneFood, addFood, updateFood, deleteFood } = require("../controller/data.controller")
+const { getAllFood, getOneFood, addFood, updateFood, deleteFood, getAllcities, getOnecity, addcities, updatecities, deletecities } = require("../controller/data.controller")
 
 const dataRouter = Router()
 
@@ -9,4 +9,14 @@ dataRouter.post("/add_food", addFood)
 dataRouter.post("/update_food/:id", updateFood)
 dataRouter.post("/delete_food/:id", deleteFood)
 
+
+
+
+//                                                 qoshimcha
+
+dataRouter.get("/get_all_cities", getAllcities)
+dataRouter.get("/get_one_city/:id", getOnecity)
+dataRouter.post("/add_city", addcities )
+dataRouter.put("/update_city/:id", updatecities)
+dataRouter.delete("/delete_city/:id", deletecities)
 module.exports = dataRouter
